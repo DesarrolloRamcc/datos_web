e65a992c871af90e0edaa1c308358db39a5eda90
giugerlo <ggiuliano526@gmail.com>
2025-02-13 12:30:51 -0300

    agrego en nav el nombre del municipio

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,9 @@ $current_page = basename($_SERVER['PHP_SELF'], '.php');
 ?>
 
 <header id="header" class="header d-flex align-items-center sticky-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
 
-        <a href="index.php" class="logo d-flex align-items-center me-auto">
+        <a href="index.php" class="logo d-flex align-items-center">
             <img src="assets/img/ramcc.png" alt="Logo RAMCC" class="img-fluid">
             <!-- <h1 class="sitename"><b>Sistema de carga</b></h1> -->
         </a>
@@ -21,13 +21,18 @@ $current_page = basename($_SERVER['PHP_SELF'], '.php');
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <?php if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']): ?>
-            <a class="btn-getstarted" href="InicioDeSesion">Ingresar</a>
-        <?php else: ?>
-            <a href="logout.php" class="btn-logout">
-                <i class="bi bi-box-arrow-right me-2"></i>Cerrar Sesión
-            </a>
-        <?php endif; ?>
+        <div class="d-flex align-items-center">
+            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
+                <?php if (isset($_SESSION['id_municipio']) && $_SESSION['id_municipio'] != null && $_SESSION['id_municipio'] != 0): ?>
+                    <span class="me-3 fw-bold">Hola, <span class="text-success"><?php echo htmlspecialchars($_SESSION['nombre_municipio']); ?></span></span>
+                <?php endif; ?>
+                <a href="logout.php" class="btn-logout">
+                    <i class="bi bi-box-arrow-right me-2"></i>Cerrar Sesión
+                </a>
+            <?php else: ?>
+                <a class="btn-getstarted" href="InicioDeSesion">Ingresar</a>
+            <?php endif; ?>
+        </div>
 
     </div>
 </header>
